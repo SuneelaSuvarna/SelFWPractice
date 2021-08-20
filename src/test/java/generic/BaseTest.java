1bd2aa4db406235b657ea3566d7a9b38977769f5
Suneela <suneela@Suneela-MBP.local>
2021-08-20 19:52:30 +0530

    Aug20

--- src/test/java/generic/BaseTest.java
+++ src/test/java/generic/BaseTest.java
@@ -1,12 +1,18 @@
 package generic;
 
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Optional;
+import org.testng.annotations.Parameters;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
@@ -15,11 +21,19 @@ public class BaseTest implements IConst {
 	public WebDriver driver;
 	public WebDriverWait wait;
 
+	@Parameters({ "hubUrl", "browser" })
 	@BeforeMethod
-	public void openApp() {
-		WebDriverManager.chromedriver().setup();
-		driver = new ChromeDriver();
-		wait=new WebDriverWait(driver,ETO);
+	public void openApp(@Optional(dURL) String hubUrl, @Optional(dBrowser) String browser)
+			throws MalformedURLException {
+		// WebDriverManager.chromedriver().setup();
+		// driver = new ChromeDriver();
+
+		// Grid Integration
+		URL remoteURL = new URL(hubUrl);
+		DesiredCapabilities cap = new DesiredCapabilities();
+		cap.setBrowserName(browser);
+		driver = new RemoteWebDriver(remoteURL, cap);
+		wait = new WebDriverWait(driver, ETO);
 		driver.manage().timeouts().implicitlyWait(ITO, TimeUnit.SECONDS);
 		driver.manage().window().maximize();
 		driver.get(APP_URL);
--- src/test/java/generic/IConst.java
+++ src/test/java/generic/IConst.java
@@ -5,5 +5,7 @@ public interface IConst {
 	 int ITO = 10;
 	 int ETO = 10;
 	 String APP_URL = "https://demo.actitime.com/login.do";
-	 String XL_PATH=System.getProperty("user.dir") + "/Data/TestData2.xlsx";
+	 String XL_PATH=System.getProperty("user.dir") + "/Data/InputData.xlsx";
+	 String dURL="http://localhost:4444/wd/hub";
+	 String dBrowser="chrome";
 }
